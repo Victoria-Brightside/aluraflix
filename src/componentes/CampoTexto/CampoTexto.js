bffe8b5e3e9d6d2e725337521cb59e5aab87ac17
Seier <seier.bernal@gmail.com>
2024-07-03 18:27:12 -0600

    Se agrega botón de envio a formulario

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -2,9 +2,8 @@ import "./CampoTexto.css"
 const CampoTexto = (props) => {
     return <div className="campo-texto">
         <label> {props.titulo} </label>
-        <input placeholder={props.placeholder} />
+        <input placeholder={props.placeholder} required={props.required}/>
         </div>
 
 }
-
 export default CampoTexto
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,23 +4,21 @@ import ListaOpciones from "../ListaOpciones/ListaOpciones"
 import Boton from "../Boton/Boton"
 
 const Formulario = () => {
+
+const manejarEnvio = (evento) =>{
+    evento.preventDefault()
+}
+    
     return <section className="formulario">
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h1> NUEVO VIDEO </h1>
             <h3> COMPLETE EL FORMULARIO PARA CREAR UNA NUEVA TARJETA DE VIDEO</h3>
-<<<<<<< Updated upstream
-        <h2>Crear Tarjeta</h2>
-        <CampoTexto titulo="Título" placeholder="Ingrese título"/>
-        <ListaOpciones titulo="Categoría" placeholder="Seleccione una categoría"/>
-        <CampoTexto titulo="Video" placeholder="Ingrese el enlace del video"/>
- 
-=======
             <h2>Crear Tarjeta</h2>
-            <CampoTexto titulo="Título" placeholder="Ingrese título" />
-            <ListaOpciones titulo="Categoría" placeholder="Seleccione una categoría" />
-            <CampoTexto titulo="Video" placeholder="Ingrese el enlace del video" />
-            <Boton />
->>>>>>> Stashed changes
+            <CampoTexto titulo="Título" placeholder="Ingrese título" required/>
+            <ListaOpciones titulo="Categoría" placeholder="Seleccione una categoría" required />
+            <CampoTexto titulo="Video" placeholder="Ingrese el enlace del video" required/>
+            <Boton texto="Enviar"/>
+            <Boton texto="Limpiar"/>
         </form>
     </section>
 }
