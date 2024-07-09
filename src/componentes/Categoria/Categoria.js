b5b7bd08136eade99e520adacf74a8f2c5175d4d
Seier <seier.bernal@gmail.com>
2024-07-09 14:18:10 -0600

    Se agrega componente Categoria a Home

---
+++ src/componentes/Categoria/Categoria.js
@@ -0,0 +1,14 @@
+import "./Categoria.css"
+
+function Categoria(props) {
+    return <section className="supercategoria">
+        <h2> {props.datos.titulo} </h2>
+        <div className="card">
+
+        </div>
+    </section>
+
+}
+
+export default Categoria
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,33 @@
 import Header from '../componentes/Header/Header.js'
+import Categoria from '../componentes/Categoria/Categoria.js'
 
+
+const temas = [
+    {
+        titulo:"Judo",
+        colorPrimario:"green",
+        colorSecundario:"#28b463",
+    },
+    {
+        titulo:"Brazilian Jiu jitsu",
+        colorPrimario:"blue",
+        colorSecundario:" #2e86c1 ",
+    },
+    {
+        titulo:"Wrestling",
+        colorPrimario:"orange",
+        colorSecundario:"#e67e22",
+    }
+];
 
 const Home = () => {
     return (
       <main>
-    
-        
+  {
+temas.map( (tema) => {
+return <Categoria datos={tema} key={tema.titulo}/>
+      })
+  }
       </main>
     )
   }
